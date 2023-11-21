f51dbd6f32c719353b7dba52bba4225df4d8d804
Ashutosh Jena <ashutosh.jena@finbox.in>
2023-11-21 19:39:52 +0530

    Update SessionRequest

--- FinBoxLending/models/SessionRequest.swift
+++ FinBoxLending/models/SessionRequest.swift
@@ -8,19 +8,6 @@
 import Foundation
 
 struct SessionRequest: Hashable, Codable {
-    
-    let customerID: String
-    let apiKey: String
-    let withDrawAmount: Float
-    let redirectURL: String
-    let transactionID: String
-    let source: String
-    let hideClose: Bool
-    let hideFAQ: Bool
-    let hideBack: Bool
-    let hideNav: Bool
-    let hidePoweredBy: Bool
-    let sdkType: String
-    
+    let customerID: String?
+    let redirectURL: String?
 }
-
