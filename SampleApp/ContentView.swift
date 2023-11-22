d5fdf1e8a09aaf627849e7f3fd2fdebe1b096d1a
Srikar Reddy <gsrikar2010@gmail.com>
2023-11-22 06:33:16 +0530

    FLA-1850 Navigate to LendingView when clicked on Lending Button

--- SampleApp/ContentView.swift
+++ SampleApp/ContentView.swift
@@ -9,18 +9,37 @@ import SwiftUI
 import FinBoxLending
 
 struct ContentView: View {
+    
+    @State var isLendingScreenShown = false
+    
     var body: some View {
-        VStack {
-//            Button("Start Lending") {
-//                startLending()
-//            }
-            LendingView()
+        NavigationStack {
+            VStack {
+                NavigationLink(destination: startLending(), isActive: $isLendingScreenShown) {
+                    lendingButtonClicked()
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func lendingButtonClicked() -> some View {
+        Button(action: {
+            isLendingScreenShown = true
+        }, label: {
+            Text("Start Lending")
+        }).buttonStyle(.borderedProminent)
     }
     
     func startLending() -> some View {
-        LendingView()
+        let _ = FinBoxLending.Builder()
+            .apiKey(key: "iUJT1sxksi5ipCye69OTf3b8FCsQlYgl9J6SXRFY")
+            .customerID(id: "Nexarc_test_user_119109239944")
+            .userToken(token: "AuxlyTKMxkIylsMmEoeoNCocevmkMPvjZlssbSEFKDNZmbcjhAvXDoMWWjtyDUFI")
+            .environment(env: "UAT")
+            .build()
+        
+        return LendingView().navigationBarHidden(true)
     }
 }
 
