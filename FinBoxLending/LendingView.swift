4719ba69515c2db1272cd651630afe84a30f0196
Srikar Reddy <gsrikar2010@gmail.com>
2023-11-22 06:29:48 +0530

    FLA-1850 Add Builder to Preview and Remove it from body

--- FinBoxLending/LendingView.swift
+++ FinBoxLending/LendingView.swift
@@ -16,13 +16,6 @@ public struct LendingView: View {
     }
     
     public var body: some View {
-        let _ = FinBoxLending.Builder()
-            .apiKey(key: "iUJT1sxksi5ipCye69OTf3b8FCsQlYgl9J6SXRFY")
-            .customerID(id: "Nexarc_test_user_119109239944")
-            .userToken(token: "AuxlyTKMxkIylsMmEoeoNCocevmkMPvjZlssbSEFKDNZmbcjhAvXDoMWWjtyDUFI")
-            .environment(env: "UAT")
-            .creditLineAmount(amount: "500000")
-            .build()
         VStack {
             FinBoxWebView(urlString: "https://lendingwebuat.finbox.in/session/985573d3-b5c8-4442-9810-ab249f669b43?hidePoweredBy=false")
         }.onAppear(perform: {
@@ -32,6 +25,12 @@ public struct LendingView: View {
     
     struct LendingView_Previews: PreviewProvider {
         static var previews: some View {
+            let _ = FinBoxLending.Builder()
+                .apiKey(key: "iUJT1sxksi5ipCye69OTf3b8FCsQlYgl9J6SXRFY")
+                .customerID(id: "Nexarc_test_user_119109239944")
+                .userToken(token: "AuxlyTKMxkIylsMmEoeoNCocevmkMPvjZlssbSEFKDNZmbcjhAvXDoMWWjtyDUFI")
+                .environment(env: "UAT")
+                .build()
             LendingView()
         }
     }
