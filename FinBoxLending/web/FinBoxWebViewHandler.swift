6374f0bde8e16436259053d5aa95ca5071f93e9e
Srikar Reddy <gsrikar2010@gmail.com>
2023-11-27 15:43:28 +0530

    FLA-1853 Parse Event Response from Webview

--- FinBoxLending/web/FinBoxWebViewHandler.swift
+++ FinBoxLending/web/FinBoxWebViewHandler.swift
@@ -17,29 +17,36 @@ class FinBoxWebViewHandler: NSObject, WKScriptMessageHandler {
     init(lendingResult: @escaping (FinBoxJourneyResult) -> Void) {
         self.lendingResult = lendingResult
     }
-
+    
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         do {
             // Parse the message body received from webview
-            try parseMessageBody(message: message.body)
+            // Serialize the message data
+            let jsonData = try JSONSerialization.data(withJSONObject: message.body)
+            // Convert the web response event to struct
+            let eventResponse = try JSONDecoder().decode(WebEventResponse.self, from: jsonData)
         } catch {
             print("Json Decode Error")
         }
-
+        
     }
     
     // Parse the message body from the webview event
-    func parseMessageBody(message: Any) throws {
-        // TODO: Parse the response sent
-        // TODO: call setCallbackPayload method
+    func parseMessageBody(eventResponse: WebEventResponse) throws {
+        if (eventResponse.status == "PERSONAL_INFO_SUBMITTED") {
+            // Personal information is submitted
+        } else {
+            // Send the callback information
+            setCallbackPayload(eventResponse: eventResponse.data)
+        }
     }
     
     // Update the callback payload
-    func setCallbackPayload(message: String?, eventResponse: FinBoxJourneyResult?) {
+    func setCallbackPayload(eventResponse: FinBoxJourneyResult?) {
         // Generate the callback payload
-        let payload = FinBoxJourneyResult(code: "NOT_SET", screen: "NOT_SET", message: "NOT_SET")
+        let payload = FinBoxJourneyResult(code: eventResponse?.code, screen: eventResponse?.screen, message: eventResponse?.message)
         debugPrint("Callback Entity Id", payload.code ?? "Empty Status Code")
-
+        
         // Send callback to the View
         self.lendingResult(payload)
     }
