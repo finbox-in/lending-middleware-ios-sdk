0e6d70e995548612c28ee0aac8ac48520484b371
Ashutosh Jena <ashutosh.jena@finbox.in>
2023-11-30 15:35:29 +0530

    FLA-1859 Update Constants to Global Constants

--- FinBoxLending/constants/Constants.swift
+++ FinBoxLending/constants/Constants.swift
@@ -7,25 +7,23 @@
 
 import Foundation
 
-struct Constants {
-    // User default keys set during initialization
-    static let FINBOX_LENDING_CUSTOMER_ID = "finbox_lending_customer_id"
-    static let FINBOX_LENDING_API_KEY = "finbox_lending_api_key"
-    static let FINBOX_LENDING_USER_TOKEN = "finbox_lending_user_token"
-    static let FINBOX_LENDING_ENVIRONMENT = "finbox_lending_environment"
-    static let FINBOX_LENDING_CREDIT_LINE_AMOUNT = "finbox_lending_credit_line_amount"
-    static let FINBOX_LENDING_CREDIT_LINE_TRANSACTION_ID = "finbox_lending_credit_line_transaction_id"
-    static let FINBOX_LENDING_SHOW_TOOL_BAR = "finbox_lending_show_tool_bar"
-    static let FINBOX_LENDING_HIDE_POWERED_BY = "finbox_lending_hide_powered_by"
+// User default keys set during initialization
+let FINBOX_LENDING_CUSTOMER_ID = "finbox_lending_customer_id"
+let FINBOX_LENDING_API_KEY = "finbox_lending_api_key"
+let FINBOX_LENDING_USER_TOKEN = "finbox_lending_user_token"
+let FINBOX_LENDING_ENVIRONMENT = "finbox_lending_environment"
+let FINBOX_LENDING_CREDIT_LINE_AMOUNT = "finbox_lending_credit_line_amount"
+let FINBOX_LENDING_CREDIT_LINE_TRANSACTION_ID = "finbox_lending_credit_line_transaction_id"
+let FINBOX_LENDING_SHOW_TOOL_BAR = "finbox_lending_show_tool_bar"
+let FINBOX_LENDING_HIDE_POWERED_BY = "finbox_lending_hide_powered_by"
     
-    // FinBox Lending Callback Statuses
-    static let FINBOX_LENDING_PERSONAL_INFO_SUBMITTED = "PERSONAL_INFO_SUBMITTED"
-    static let FINBOX_LENDING_EXIT = "EXIT"
-    static let FINBOX_LENDING_APPLICATION_COMPLETED = "APPLICATION_COMPLETED"
-    static let FINBOX_LENDING_PAYMENT_SUCCESSFULL = "PAYMENT_SUCCESSFULL"
-    static let FINBOX_LENDING_WAIT = "WAIT"
-    static let FINBOX_LENDING_OTP_LIMIT_EXCEEDED = "OTP_LIMIT_EXCEEDED"
-    static let FINBOX_LENDING_LOCATION_PERMISSION = "ESIGN_LOCATION_PERMISSION"
-    static let FINBOX_LENDING_SHOW_PROFILE_ICON = "SHOW_PROFILE_ICON"
-    static let CAMERA_PERMISSION = "CAMERA_PERMISSION"
-}
+// FinBox Lending Callback Statuses
+let FINBOX_LENDING_PERSONAL_INFO_SUBMITTED = "PERSONAL_INFO_SUBMITTED"
+let FINBOX_LENDING_EXIT = "EXIT"
+let FINBOX_LENDING_APPLICATION_COMPLETED = "APPLICATION_COMPLETED"
+let FINBOX_LENDING_PAYMENT_SUCCESSFULL = "PAYMENT_SUCCESSFULL"
+let FINBOX_LENDING_WAIT = "WAIT"
+let FINBOX_LENDING_OTP_LIMIT_EXCEEDED = "OTP_LIMIT_EXCEEDED"
+let FINBOX_LENDING_LOCATION_PERMISSION = "ESIGN_LOCATION_PERMISSION"
+let FINBOX_LENDING_SHOW_PROFILE_ICON = "SHOW_PROFILE_ICON"
+let CAMERA_PERMISSION = "CAMERA_PERMISSION"
