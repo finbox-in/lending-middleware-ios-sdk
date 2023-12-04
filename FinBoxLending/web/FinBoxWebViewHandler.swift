54d2b2269d8df31e231981dcb64c03f014a2a9fb
Ashutosh Jena <ashutosh.jena@finbox.in>
2023-12-04 23:11:02 +0530

    FLA-1857 Handle Callbacks

--- FinBoxLending/web/FinBoxWebViewHandler.swift
+++ FinBoxLending/web/FinBoxWebViewHandler.swift
@@ -27,7 +27,6 @@ class FinBoxWebViewHandler: NSObject, WKScriptMessageHandler {
         
     }
     
-    // Parse the message body from the webview event
     func parseMessageBody(message: Any) throws {
         // Parse the message body received from webview
         debugPrint("Message Body", message)
@@ -39,26 +38,79 @@ class FinBoxWebViewHandler: NSObject, WKScriptMessageHandler {
         let response = try JSONDecoder().decode(WebEventResponse.self, from: jsonData)
         debugPrint("Json Data Status", response.status ?? "Status is empty")
         
-        if (response.status == "PERSONAL_INFO_SUBMITTED") {
-            // Personal information is submitted
-        } else {
-            // Send the callback information
-            try setCallbackPayload(eventResponse: getEventResponse(response: response.data))
-        }
+        try setCallbackPayload(eventResponse: getEventResponse(webEventResponse: response))
     }
     
     // Takes event response in string format and return it as a struct
-    func getEventResponse(response: String?) throws -> FinBoxJourneyResult {
-        // Convert the json string to json object
-        let data = response?.data(using: .utf8)
-        let jsonObjectData = try JSONSerialization.jsonObject(with: data!, options : .allowFragments)
+    func getEventResponse(webEventResponse: WebEventResponse?) throws -> FinBoxJourneyResult? {
         
-        // Convert the web response event to struct
-        let jsonData = try JSONSerialization.data(withJSONObject: jsonObjectData)
-        let response = try JSONDecoder().decode(FinBoxJourneyResult.self, from: jsonData)
+        var finBoxJourneyResult = FinBoxJourneyResult(
+            code: nil,
+            screen: nil,
+            message: nil
+        )
+        
+        debugPrint("Result Response: \(webEventResponse)")
+        
+        switch (webEventResponse?.status) {
+            case FINBOX_LENDING_PERSONAL_INFO_SUBMITTED:
+                debugPrint("Personal info submitted")
+                // TODO: Check this later
+                
+            case FINBOX_LENDING_EXIT:
+                debugPrint("User Exit")
+                finBoxJourneyResult.code = "MW500"
+                finBoxJourneyResult.message = "User Exit"
+                
+            case FINBOX_LENDING_APPLICATION_COMPLETED:
+                debugPrint("Lending Result Success")
+                finBoxJourneyResult.code = "MW200"
+                finBoxJourneyResult.message = "Application Completed"
+                
+            case FINBOX_LENDING_WAIT:
+                debugPrint("Lending Result Wait")
+                // TODO: Check this later
+                finBoxJourneyResult.code = "MW400"
+                
+            case FINBOX_LENDING_PAYMENT_SUCCESSFULL:
+                debugPrint("Lending Payment Successful")
+                guard let dataString = webEventResponse?.data else {
+                    break
+                }
+                
+                let data = Data(dataString.utf8)
+                
+                let jsonObjectData = try JSONSerialization.jsonObject(with: data, options : .allowFragments)
+                
+                // Convert the web response event to struct
+                let jsonData = try JSONSerialization.data(withJSONObject: jsonObjectData)
+                let response = try JSONDecoder().decode(FinBoxJourneyResult.self, from: jsonData)
+                debugPrint("Obj received: \(response)")
+                
+            case FINBOX_LENDING_OTP_LIMIT_EXCEEDED:
+                debugPrint("Lending OTP Limit Exceeded")
+                finBoxJourneyResult.code = FINBOX_RESULT_CODE_CREDIT_LINE_FAILURE
+                finBoxJourneyResult.message = "OTP Limit Exceeded"
+                
+            case FINBOX_LENDING_LOCATION_PERMISSION:
+                debugPrint("Lending Location Permission Requested")
+                // TODO: Check this later
+                
+            case FINBOX_LENDING_SHOW_PROFILE_ICON:
+                debugPrint("Lending Show Profile Icon")
+                // TODO: Check this later
+                
+            case CAMERA_PERMISSION:
+                debugPrint("Lending Camera Permission Requested")
+                // TODO: Check this later
+                
+            default:
+                // Send the callback information
+                debugPrint("Default case")
+        }
         
         // Create the journey result
-        return FinBoxJourneyResult(code: response.code, screen: response.screen, message: response.message)
+        return finBoxJourneyResult
     }
     
     // Update the callback payload
