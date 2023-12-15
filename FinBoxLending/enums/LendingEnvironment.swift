b425e62896e42005237e316107158f30c3201e92
Ashutosh Jena <ashutosh.jena@finbox.in>
2023-12-15 15:26:20 +0530

    FLA-1864 Add LendingEnvironment Enum

---
+++ FinBoxLending/enums/LendingEnvironment.swift
@@ -0,0 +1,12 @@
+//
+//  LendingEnvironment.swift
+//  FinBoxLending
+//
+//  Created by Ashutosh Jena on 15/12/23.
+//
+
+import Foundation
+
+public enum LendingEnvironment {
+    case DEV, UAT, PROD
+}
