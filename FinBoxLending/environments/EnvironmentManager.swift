438ea099d04cd5f5a8e52a00debe84da541cdef7
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-01-18 18:39:33 +0530

    FLA-1871 Update EnvironmentManager to use Strings

--- FinBoxLending/environments/EnvironmentManager.swift
+++ FinBoxLending/environments/EnvironmentManager.swift
@@ -17,7 +17,7 @@ class EnvironmentManager {
     private init() {}
     
     // Method to get the environment from UserDefaults
-    func getEnvironment() -> LendingEnvironment? {
+    func getEnvironment() -> String {
         return FinBoxLendingPref().environment
     }
     
@@ -26,18 +26,52 @@ class EnvironmentManager {
         var baseURL: String = ""
         let environment = getEnvironment()
         
-        #if DEBUG
-        if (environment == LendingEnvironment.DEV) {
-            baseURL = FINBOX_LENDING_DEV_BASE_URL
-        }
-        #endif
+//        if (environment == "PROD") {
+//            baseURL = FINBOX_LENDING_PROD_BASE_URL
+//        } else {
+//            return
+//        }
         
-        if (environment == LendingEnvironment.UAT) {
-            baseURL = FINBOX_LENDING_UAT_BASE_URL
-        } else if (environment == LendingEnvironment.PROD) {
-            baseURL = FINBOX_LENDING_PROD_BASE_URL
-        }
+        return processEnvironment(environment: environment)
         
         return baseURL
     }
+    
+    private func processEnvironment(environment: String) -> String {
+        var url = "https://lendinguat.finbox.in"
+        let envAndRegion = environment.uppercased()
+
+        // Pass 1: Extract Environment ("UAT", "DEV", "PROD")
+        let envRegex = try? NSRegularExpression(pattern: "^(DEV|UAT|PROD)", options: .caseInsensitive)
+        if let envMatch = envRegex?.firstMatch(in: envAndRegion, options: [], range: NSRange(location: 0, length: envAndRegion.utf16.count)) {
+            let envResult = (envAndRegion as NSString).substring(with: envMatch.range).lowercased()
+
+            if envResult == "prod" { return url }
+
+            if envResult == "uat" { print("UAT") }
+
+            if envResult == "dev" {
+                print("DEV")
+                #if DEBUG
+                // Do Nothing
+                #else
+                return url
+                #endif
+            }
+            
+
+            // Pass 2: Extract Region (Numbers)
+            let regionRegex = try? NSRegularExpression(pattern: "\\d{1,2}$", options: .caseInsensitive)
+            if let regionMatch = regionRegex?.firstMatch(in: envAndRegion, options: [], range: NSRange(location: 0, length: envAndRegion.utf16.count)) {
+                let regionResult = (envAndRegion as NSString).substring(with: regionMatch.range)
+
+                // Combine results
+                url = "https://lending\(envResult)\(regionResult).finbox.in"
+            }
+        }
+
+        return url
+    }
 }
+
+
