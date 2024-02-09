45e249502048b43621bb2579aec5f5d872194b14
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-02-09 12:06:06 +0530

    FLA-1874 Update View Handling

--- FinBoxLending/viewmodels/SessionViewModel.swift
+++ FinBoxLending/viewmodels/SessionViewModel.swift
@@ -10,11 +10,12 @@ import Foundation
 class SessionViewModel: ObservableObject {
     
     @Published var sessionUrl: String?
+    var sessionFetched = false
     
     func fetchSession() {
-        debugPrint("Fetching Session")
         APIService.shared.fetchSession { result in
             debugPrint("Received Session in viewmodel")
+            self.sessionFetched = true
             DispatchQueue.main.async {
                 // Update sessionUrl when the network call completes
                 self.sessionUrl = result
