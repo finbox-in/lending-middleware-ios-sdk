0303e11ce8e9adcfe42fc713d9fe2bf38df2f82e
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-02-12 17:12:50 +0530

    FLA-1875 Update View Handling in LendingView

--- FinBoxLending/LendingView.swift
+++ FinBoxLending/LendingView.swift
@@ -19,36 +19,48 @@ public struct LendingView: View {
     }
     
     public var body: some View {
-        VStack {
-            if (viewModel.sessionUrl != nil) {
-                // Load the webpage
-                FinBoxWebView(urlString: viewModel.sessionUrl, lendingResult: lendingResult)
-            } else {
-                if #available(iOS 14, *) {
-                    // Show progress
-                    ProgressView()
+        if viewModel.sessionFetched {
+            VStack {
+                if viewModel.sessionResult?.error == nil {
+                    if viewModel.sessionResult?.sessionURL != nil {
+                        FinBoxWebView(urlString: viewModel.sessionResult?.sessionURL, lendingResult: lendingResult)
+                    } else {
+                        handleError(error: "Invalid session url")
+                    }
                 } else {
-                    // Progress View is not available
+                    handleError(error: viewModel.sessionResult?.error ?? "Unknown Error")
                 }
+            }.onAppear() {
+                debugPrint("Session Fetched", viewModel.sessionFetched)
             }
-        }.onAppear(perform: {
-            viewModel.fetchSession()
-        })
-    }
-
-}
-
-struct LendingView_Previews: PreviewProvider {
-    static var previews: some View {
-        let _ = FinBoxLending.Builder()
-            .apiKey(key: "f078b4d0-d171-4ed9-a1ce-c02134213b6c")
-            .customerID(id: "demo_lender_user_11281543")
-            .userToken(token: "RmvdboEyIfgpCwGlHinwWhhCTnQgQiwIkxLyXosjGZBqLOFDcDfDFcdhxTviyqOc")
-            .environment(env: "UAT")
-            .build()
-        LendingView() {
-            payload in
-            debugPrint("Status Code", payload.code ?? "Status Code is empty")
+        } else {
+            VStack {
+                ProgressView()
+            }.onAppear() {
+                debugPrint("Fetching Session")
+                viewModel.fetchSession()
+            }
         }
     }
+    
+    func handleError(error: String) -> some View {
+        lendingResult(FinBoxJourneyResult(code: "", screen: "", message: error))
+        return Text("\(String(describing: error))")
+    }
+
 }
+
+//struct LendingView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        let _ = FinBoxLending.Builder()
+//            .apiKey(key: "f078b4d0-d171-4ed9-a1ce-c02134213b6c")
+//            .customerID(id: "demo_lender_user_11281543")
+//            .userToken(token: "RmvdboEyIfgpCwGlHinwWhhCTnQgQiwIkxLyXosjGZBqLOFDcDfDFcdhxTviyqOc")
+//            .environment(env: "UAT")
+//            .build()
+//        LendingView() {
+//            payload in
+//            debugPrint("Status Code", payload.code ?? "Status Code is empty")
+//        }
+//    }
+//}
