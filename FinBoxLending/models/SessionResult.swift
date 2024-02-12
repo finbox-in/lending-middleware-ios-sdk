5dd4b6182be3a76b77740e9c849fc234dc08fd8d
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-02-12 16:11:24 +0530

    FLA-1875 Add SessionResult

---
+++ FinBoxLending/models/SessionResult.swift
@@ -0,0 +1,14 @@
+//
+//  SessionResult.swift
+//  FinBoxLending
+//
+//  Created by Ashutosh Jena on 20/11/23.
+//
+
+import Foundation
+
+/// Data holder Struct for ViewModel
+struct SessionResult {
+    let error: String?
+    let sessionURL: String?
+}
